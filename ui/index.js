6155489bc1c161060f1bc9d5834150d4c67875b3
chejen <jkopre.qek@gmail.com>
2022-04-02 11:32:20 +0800

    add: [Lit] first component

--- ui/index.js
+++ ui/index.js
@@ -1,1 +1,14 @@
-console.log('test-index');
+import { LitElement, html } from 'lit';
+
+/** Custom `time-deposit` component */
+class TimeDeposit extends LitElement {
+  /**
+   * To define a template for `time-deposit`
+   * @return {TemplateResult} template result
+   */
+  render() {
+    return html`<p>Time deposit</p>`;
+  }
+}
+
+customElements.define('time-deposit', TimeDeposit);
